6952e7973ad9811e58468acf1319b24e386ea933
18114290034 <18114290034@OLAB04-D17.academico.iesb.br>
2019-10-16 21:11:27 -0300

    Criando Projeto Atividade 14

---
+++ Atividade14/src/java/managedbean/ControladorMB.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbean;
+
+import ejb.StatelessEJB;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author 18114290034
+ */
+   @Named(value = "controladorMB")
+   @RequestScoped
+public class ControladorMB implements Serializable {
+
+    public ControladorMB() {
+        
+        System.out.println("Criando controlador...");
+    }
+    
+                 
+       @EJB
+       private StatelessEJB ejb;
+       
+       
+
+    public StatelessEJB getEjb() {
+        return ejb;
+    }
+
+    public void setEjb(StatelessEJB ejb) {
+        this.ejb = ejb;
+    }
+       
+}
