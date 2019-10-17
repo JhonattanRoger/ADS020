2df5fde818d86a1d6779d141646bb6f6824b83d6
18114290034 <18114290034@OLAB04-D17.academico.iesb.br>
2019-10-16 21:51:43 -0300

    Criando Projeto Atividade 15

---
+++ Atividade15/src/java/ejb/StatefulEJB.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
+import javax.ejb.Stateful;
+
+/**
+ *
+ * @author 18114290034
+ */
+@Stateful
+public class StatefulEJB implements Serializable {
+    
+    private int contador = 0;
+
+    public StatefulEJB() {
+        System.out.println("Criando o stateful...");
+    }
+    
+    public int invocar(){
+        System.out.println("Invocando...");
+        return ++contador;
+    }
+    
+    @PostConstruct
+    public void iniciar(){
+        System.out.println("Iniciando o stateful...");
+    }
+    
+    @PostActivate
+    public void ativar(){
+        System.out.println("Ativando o stateful...");
+    }
+    
+    @PrePassivate
+    public void desativar(){
+        System.out.println("Desativando o stateful...");
+    }
+    
+    @PreDestroy
+    public void finalizar(){
+        System.out.println("Finalizando o stateful...");
+    }
+    
+    
+}
